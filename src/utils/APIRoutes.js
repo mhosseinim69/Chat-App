10802e137b1b375be8f77c639089613640a0a2ff
Mostafa Hosseini <m.hosseini.m69@gmail.com>
2023-02-22 11:09:31 +0330

    add url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://chat-api.veresk.cloud:3000/";
+export const host = "https://chat-app-api-qee4.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
